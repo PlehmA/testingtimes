d5ddb99e11d3ff0518acb80f0d15b5694aa94bc1
Andres Plehm <andres@diwait.com>
2021-09-16 16:13:15 -0300

    .

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tablita para los whaches</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4188818814719897"
+     crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="container">
