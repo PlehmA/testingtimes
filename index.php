27ac1b3321fd6e1545c011de86596f5f906663b1
Andres Plehm <andres@diwait.com>
2021-09-17 08:53:25 -0300

    .

--- index.php
+++ index.php
@@ -31,8 +31,8 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
 
 $interval = 1800; // Interval in seconds
 
-$date_first     = date('Y-m-d')." 01:15";
-$date_second    = date('Y-m-d')." 23:45";
+$date_first     = date('Y-m-d')." 01:16";
+$date_second    = date('Y-m-d')." 23:46";
 
 $time_first     = strtotime($date_first);
 $time_second    = strtotime($date_second);
