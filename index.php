88e6359a94ff67091186f95683351003ccf52e19
Andres Plehm <andres@diwait.com>
2021-09-17 18:01:57 -0300

    .

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4188818814719897"
      crossorigin="anonymous"></script>
 </head>
-<body>
+<body class="bg-dark text-white">
 <div class="container">
     <h1 class="text-center">Horarios de Argentina</h1>
 <table class="table table-sm table-bordered">
@@ -43,9 +43,9 @@ for ($i = $time_first; $i < $time_second; $i += $interval){
     }
 $g++;
 if (date('H:i') >= date('H:i', $i) &&  date('H:i') <= date('H:i', strtotime('+29 minute' ,$i))) {
-    echo "<td class='text-center'><span class='badge bg-dark'>".date('H:i', $i)."</span></td>";
+    echo "<td class='text-center'><span class='badge bg-light text-dark'>".date('H:i', $i)."</span></td>";
 }else {
-    echo "<td class='text-center'>".date('H:i', $i)."</td>";
+    echo "<td class='text-center text-white'>".date('H:i', $i)."</td>";
 }
     
     if ($g == 9) {
